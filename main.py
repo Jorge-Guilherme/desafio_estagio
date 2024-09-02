3d515d64c4ab47b65f394b9417cd7c79497423d0
Jorge-Guilherme <jorge.guilherme784@gmail.com>
2024-09-02 11:28:56 -0300

    Primeira Questão

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def fibonacci(n):
+def fibonacci(n): # função para gerar a sequência
     if n <= 1:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
     
-def gerar_sequencia(n, seq=None):
+def gerar_sequencia(n, seq=None): # função para gerar a sequência até o valor inserido "n"
     if seq is None:
         seq = [0]
     if seq[-1] < n:
@@ -13,7 +13,7 @@ def gerar_sequencia(n, seq=None):
         return gerar_sequencia(n, seq)
     return seq
 
-def verificar_numero(n):
+def verificar_numero(n): # verificar se o valor "n" está entre os gerados
     fib_seq = gerar_sequencia(n)
     if n in fib_seq:
         return f"O número {n} pertence à sequência de Fibonacci."
